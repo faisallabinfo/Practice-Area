f9a44d25e816955a4b0e221f0b30b0903329aeee
Faisal Rahman <mailtofaisalrahman@gmail.com>
2023-11-02 17:38:08 +0530

    Deleted assignment changes

--- my_first_app/src/app/warningalert/warninglalert.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector:'app-warninglalert',
-    templateUrl:'./warninglalert.component.html',
-    styleUrls:['././warninglalert.component.css']
-})
-
-export class WarningAlert {}
