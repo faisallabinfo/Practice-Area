2e2ff745168f99b81d12a52e80d67f2ce1b2f157
Faisal Rahman <mailtofaisalrahman@gmail.com>
2023-11-03 13:06:07 +0530

    Done with 02-11-2023

--- my_first_app/src/app/app.module.ts
+++ my_first_app/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './server/server.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ServerComponent,
     ServersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
