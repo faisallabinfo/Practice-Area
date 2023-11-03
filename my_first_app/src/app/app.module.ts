17dd3a7a23b177caa6e20c1d997655ffd9a030d1
Faisal Rahman <mailtofaisalrahman@gmail.com>
2023-11-03 15:52:34 +0530

    Assignment 2 done

--- my_first_app/src/app/app.module.ts
+++ my_first_app/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServersComponent } from './servers/servers.component';
-import { ServerComponent } from './server/server.component';
 import { FormsModule } from '@angular/forms';
+import { UsernameComponent } from './username/username.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    UsernameComponent
   ],
   imports: [
     BrowserModule,
